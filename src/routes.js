29b0b07e40e92127c5a810eb3911599450d913e1
Fazrul <fazrul.anugrah17@gmail.com>
2022-07-13 14:22:58 +0800

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+// eslint-disable-next-line import/extensions
+import {
+  // eslint-disable-next-line max-len
+  addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler,
+// eslint-disable-next-line import/extensions
+} from './handler.js';
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+export default routes;
